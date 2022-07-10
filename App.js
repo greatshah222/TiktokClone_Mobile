9f614e279f7bba7b233ced7ef119ec0297c0a635
greatshah222 <great.shah222@gmail.com>
2022-07-10 23:49:34 +0300

    post component started

--- App.js
+++ App.js
@@ -1,120 +1,25 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
-  View,
+  Text,
 } from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import HomeScreen from './src/screens/HomeScreen';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import Video from 'react-native-video';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
+const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            flex: 1,
-            width: 300,
-            height: 400,
-            backgroundColor: 'red',
-          }}>
-          <Text>bIhsal shah </Text>
-
-          <Video
-            source={{
-              uri: 'https://suiterc.icareus.com/s3files/10154/3409917/267201.mp4/1.0.mp4?organizationId=3409909',
-            }}
-            style={styles.Video}
-            onError={e => console.log(e)}
-            resizeMode={'cover'}
-            onBuffer={e => console.log(e)}
-            controls
-          />
-        </View>
-      </ScrollView>
+      <HomeScreen />
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  Video: {
-    height: '100%',
-    width: '100%',
-    backgroundColor: 'blue',
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Post from '../components/Post';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Post />
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
